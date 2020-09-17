8e240fb640ddd8e9fbeb8d5f9eb662458b85943f
youse <youse@DESKTOP-ICIF3EO>
2020-09-17 18:21:13 +0900

    QuestionDAO完成  コメント追加

--- src/dao/QuestionDAO.java
+++ src/dao/QuestionDAO.java
@@ -18,6 +18,16 @@ public class QuestionDAO {
 	private final String DB_PASS = "Yousei8239";
 	Connection con = null;
 
+
+	//--------------------------------------------------------------------
+	//                      allQuestionSelect
+	//--------------------------------------------------------------------
+	//ユーザー毎の問題を全て選択し、問題のリストを返すメソッド。
+	//検索に使用されるのは、ユーザー名のみ。
+	//使用する際は、ユーザー名を格納した User インスタンスを
+	//Question インスタンスに保存し、Questionインスタンスを引数として使用。
+	//返り値は、List< Question >型の、ArrayList を返す。
+	//--------------------------------------------------------------------
 	public List<Question> allQuestionSelect( Question imputQuestion ) {
 
 		List<Question> questionList = new ArrayList<Question>();
@@ -69,7 +79,17 @@ public class QuestionDAO {
 
 		return questionList;
 	}
+	//----------------------------------------------------------ここまで
 
+
+	//------------------------------------------------------------------
+	//                      insertQuestion
+	//------------------------------------------------------------------
+	//ユーザーから入力された、問題をデータベースに保存するメソッド。
+	//使用する際は、引数に問題の String category , String answer , User user ,
+	//String hintList[] を格納した、Question インスタンスを引数とする。
+	//返り値は、 SQL にて実行された行数を int型 で返す。
+	//------------------------------------------------------------------
 	public int insertQuestion( Question imputQuestion ) {
 
 		int resultColumn = 0;
@@ -107,5 +127,97 @@ public class QuestionDAO {
 
 		return resultColumn;
 	}
+	//------------------------------------------------------ここまで
 
+
+	//--------------------------------------------------------------
+	//                     deleteQuestion
+	//--------------------------------------------------------------
+	//ユーザーが指定した、問題のIDによって、問題をデータベースから
+	//削除するメソッド。
+	//問題の一覧テーブルなどから、ユーザーが選択した問題のIDを
+	//Questionインスタンスに保存し、それを引数とする。
+	//返り値は、 SQL にて実行された行数を int型 で返す。
+	//--------------------------------------------------------------
+	public int deleteQuestion( Question imputQuestion ) {
+
+		int resultColumn = 0;
+
+		try {
+
+		Class.forName( DRIVER_NAME );
+		con = DriverManager.getConnection( JDBC_URL, DB_USER, DB_PASS );
+
+		String sql = "delete from question where id = ?";
+
+		PreparedStatement pstmt = con.prepareStatement( sql );
+
+		pstmt.setInt( 1 ,  imputQuestion.getId() );
+
+		resultColumn = pstmt.executeUpdate();
+
+		}catch( ClassNotFoundException e ) {
+			e.printStackTrace();
+		}catch( SQLException e ) {
+			e.printStackTrace();
+		}finally {
+			try {
+				con.close();
+			}catch( SQLException e ) {
+				e.printStackTrace();
+			}
+		}
+
+		return resultColumn;
+	}
+	//-----------------------------------------------------ここまで
+
+
+	//-------------------------------------------------------------
+	//                      updateQuestion
+	//-------------------------------------------------------------
+	//ユーザーが選択した問題のカテゴリー、答え、ヒントを編集するメソッド。
+	//それぞれのデータを格納した、Questionインスタンスを引数とする。
+	//更に、Questionインスタンスには、変更したい問題のIDを格納しておき
+	//それをデータベースでの、検索値とする。（該当の問題のデータを更新する)
+	//返り値には、 SQL を実行した行数を int型 で返す。
+	//-------------------------------------------------------------
+	public int updateQuestion( Question imputQuestion ) {
+
+		int resultColumn = 0;
+
+		try {
+
+			Class.forName( DRIVER_NAME );
+			con = DriverManager.getConnection( JDBC_URL, DB_USER, DB_PASS );
+
+			String sql = "update question set category = ? , answer = ? , hint_1 = ? , hint_2 = ? , hint_3 = ? where id = ?";
+
+			PreparedStatement pstmt = con.prepareStatement( sql );
+
+			pstmt.setString( 1 , imputQuestion.getCategory() );
+			pstmt.setString( 2 , imputQuestion.getAnswer() );
+			pstmt.setString( 3 , imputQuestion.getHintList()[0] );
+			pstmt.setString( 4 , imputQuestion.getHintList()[1] );
+			pstmt.setString( 5 , imputQuestion.getHintList()[2] );
+			pstmt.setInt   ( 6 , imputQuestion.getId() );
+
+			resultColumn = pstmt.executeUpdate();
+
+		}catch( ClassNotFoundException e ) {
+			e.printStackTrace();
+		}catch( SQLException e ) {
+			e.printStackTrace();
+		}finally {
+			try {
+				con.close();
+			}catch( SQLException e ) {
+				e.printStackTrace();
+			}
+		}
+
+		return resultColumn;
+	}
+
+	//---------------------------------------------------ここまで
 }
